bcc2c075bf780f20c2c3b7bfcaf9aa7ff3008125
unknown <xxberserkerxx66@gmail.com>
2021-10-26 20:43:04 +0300

    Task2

---
+++ Lesson1/Task 2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "                            _______________ " << endl;
+    cout << "                          /            @@@@@\\_ " << endl;
+    cout << "                        _/                 @@@\\_" << endl;
+    cout << "                       /                      @@\\" << endl;
+    cout << "                      /                        @@|" << endl;
+    cout << "                     /                          @|" << endl;
+    cout << "                     |                          @|" << endl;
+    cout << "                     |      QQQ        QQQ      @|_" << endl;
+    cout << "                    /       QQQ        QQQ      @@@|" << endl;
+    cout << "                    |       QQQ        QQQ       @@|" << endl;
+    cout << "                    |     TT              TT      @|" << endl;
+    cout << "                    |     TT     ___      TT       |" << endl;
+    cout << "                    |                              |" << endl;
+    cout << "                    |                              |____" << endl;
+    cout << "                    |                                @@@@\\" << endl;
+    cout << "                    |                                  @@|" << endl;
+    cout << "         _          |                                   @|" << endl;
+    cout << "        / \\________ /                                    |" << endl;
+    cout << "        |                                         ______ /" << endl;
+    cout << "        |                                       @|" << endl;
+    cout << "        \\_                                      @/" << endl;
+    cout << "          \\                                    @/"  << endl;
+    cout << "           \\___                               @/"  << endl;
+    cout << "               \\___                        ___/"  << endl;
+    cout << "                   \\ ___________________ /" << endl;
+
+    return 0;
+}
